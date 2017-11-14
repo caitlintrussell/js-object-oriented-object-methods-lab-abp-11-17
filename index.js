356f66b8edb83dd06ebf95a1e619fd681b183476
Caitlin Trussell <caitlintrussell@gmail.com>
2017-11-14 19:19:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 // `veto` — returns `No, I must disagree`
-// 
+//
 // + `approve` — returns `You can do that!`
-// 
+//
 // + `doCharity` — returns `I like to help people.`
-// 
+//
 // + `releasePressStatement` — returns `You will see great things from Scuber.`
-// 
+//
 // + `sayHi` — returns `"Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.`
 
 function BoardMember(name, homeState, training){
   this.name = name;
   this.homeState = homeState;
-  this.training = training; 
+  this.training = training;
   this.veto = function(){
     return `No, I must disagree`;
   }
   this.approve = function(){
-    return `You can do that`;
+    return `You can do that!`;
   }
   this.doCharity = function(){
     return `I like to help people.`;
